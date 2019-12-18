bef52e61f64c69e4bbfa19b68dc1a6eca83aefbb
Lakshya Thakur <lakshya.thakur@wipro.com>
2019-12-18 19:14:09 +0530

    Provider inside App.tsx fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "reactstrap";
+import { Provider } from "react-redux";
 import "./assets/global/css/Global.module.css";
+import configureStore from "./store/applicationStore";
 import ClientSummary from "./components/clientSummary/ClientSummary";
 import ErrorBoundary from "./utils/components/ErrorBoundary";
-
+const store = configureStore();
 const App: React.FC = () => (
   <>
     <ErrorBoundary>
       <Container fluid={true} className="outerContainer">
         <Container fluid={false} className="innerContainer">
-          <Router>
-            <Route exact={true} path="/" component={ClientSummary} />
-          </Router>
+          <Provider store={store}>
+            <Router>
+              <Route exact={true} path="/" component={ClientSummary} />
+            </Router>
+          </Provider>
         </Container>
       </Container>
     </ErrorBoundary>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
-import configureStore from "./store/applicationStore";
 import { renderApp } from "../src/utils/microfrontend/singleSpaHelper";
-const store = configureStore();
+
 if (process.env.NODE_ENV === "development") {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById("root")
-  );
+  ReactDOM.render(<App />, document.getElementById("root"));
 } else {
   renderApp("React", "react-app");
 }
