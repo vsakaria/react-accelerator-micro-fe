696e10cc10e620e64a035a59007084b0454f42bd
Lakshya Thakur <lakshya.thakur@wipro.com>
2019-12-23 20:58:35 +0530

    css webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,13 @@ module.exports = (
                   modules: true
                 }
               }
-            ]
+            ],
+            include: /\.module\.css$/
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+            exclude: /\.module\.css$/
           },
           {
             test: /\.(png|woff|woff2|eot|ttf|svg)$/,
