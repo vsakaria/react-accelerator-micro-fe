24b8a259788d90f7cc387345b1692c4827d3e56e
Vishal <vishal.sakaria@wipro.com>
2020-01-08 15:46:14 +0000

    ADD appName to App bootstrapping

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,23 @@ import ErrorBoundary from "./utils/components/ErrorBoundary";
 import { BrandProviderLoader } from "./utils/components/BrandProviderLoader";
 const store = configureStore();
 
-const App: React.FC = () => (
+interface App {
+  appName: string;
+}
+
+const App: React.FC<App> = props => (
   <>
     <Provider store={store}>
       <BrandProviderLoader>
         <ErrorBoundary>
           <Container fluid={true} className="outerContainer">
             <Container fluid={false} className="innerContainer">
-              <Router basename="/clientSummary">
-                <Route exact={true} path="/" component={ClientSummary} />
+              <Router basename={`/${props.appName.toLocaleLowerCase()}`}>
+                <Route
+                  exact={true}
+                  path="/clientSummary"
+                  component={ClientSummary}
+                />
               </Router>
             </Container>
           </Container>
